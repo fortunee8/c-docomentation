be5f67cf4847b8ecabccb93bd486d9b986b9f18b
Lucky Setiawan <101236568+fortunee8@users.noreply.github.com>
2022-10-20 12:02:20 +0700

    Update and add choices

--- midtest.c
+++ midtest.c
@@ -5,10 +5,14 @@ int main(void)
   int pilihan;
   int input[100], jumlah_array, i, nilai_kecil, nilai_besar, jumlah, rata; //Variabel untuk opsi 1 
   int faktorial, angka, bilangan, hasil;
+  int input_nilai; 
+  int kolom, baris, ukuran_kotak;
 
 printf("TES TENGAH SEMESTER ASDOS DDP \n");  
-printf("1. Program Nilai terkecil, terbesar, rata-rata dan jumlah \n");  
+printf("1. Program Nilai Terkecil, Terbesar, Rata-rata dan Jumlah \n");  
 printf("2. Program Faktorial \n");
+printf("3. Program Cek Nilai dan Predikat \n");
+printf("4. Program Kotak \n");
 scanf("%d", &pilihan);
 
   if(pilihan==1){
@@ -66,6 +70,47 @@ int faktorial(angka) {
   hasil = faktorial(bilangan);
   printf("Faktorial dari %d adalah %d ", bilangan, hasil);
   return 0;
+}
+
+else if (pilihan == 3){
+	printf("Masukkan nilai kamu! ");
+	scanf("%d", &input_nilai);
+	
+	if (input_nilai >= 80 && input_nilai <= 100){
+		printf("Kamu mendapatkan nilai A!");
+	}
+	else if (input_nilai >=70 && input_nilai <= 79){
+		printf("Kamu mendpatkan nilai B");
+	}
+	else if (input_nilai >= 60 && input_nilai <=69){
+		printf("Kamu mendapatkan nilai C");
+	}
+	else if (input_nilai >= 50 && input_nilai <=59){
+		printf("Kamu mendapatkan nilai D");
+	}
+	else if (input_nilai >= 0  && input_nilai <=49){
+		printf("Kamu mendapatkan nilai E");
+	}
+	else {
+		printf("Input kamu salah, coba untuk menginput nilai 1-100 menggunakan angka!");
+	}
+	
+}
+else if (pilihan == 4){
+	printf("Masukkan ukuran kotaknya! ");
+	scanf("%d", &ukuran_kotak);
+	
+	for(kolom=1;kolom<=ukuran_kotak;kolom++){
+		for(baris=1;baris<=ukuran_kotak;baris++){
+			if(baris==ukuran_kotak - kolom +1){
+				printf("* ");
+			}
+			else {
+				printf("%d",kolom);
+			}
+		}
+		printf("\n");
+	}
 }
   return 0;
 }
